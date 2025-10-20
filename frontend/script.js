d058b7dfbf59a036c7a3a8369696e4281cdc203e
Bollaboina Vamshi Yadav <130643624+vamshiyadavbollaboina@users.noreply.github.com>
2025-10-20 17:05:54 +0530

    Add files via upload

---
+++ frontend/script.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const signinButton1 = document.getElementById('signin-btn-google');
+  
+    signinButton1.addEventListener('click', function() {
+      
+      window.location.href = 'https://reachin-box-assignment.onrender.com/auth/google';
+    });
+
+    const signinButton2 = document.getElementById('signin-btn-ms');
+  
+    signinButton2.addEventListener('click', function() {
+      
+    //   window.location.href = 'https://reachinbox-assignment-4rf9.onrender.com/signin';
+    });
+  });
+  
+  // document.addEventListener('DOMContentLoaded', function() {
+  //   const signinButton1 = document.getElementById('signin-btn-google');
+  
+  //   signinButton1.addEventListener('click', function() {
+      
+  //     window.location.href = 'https://reachin-box-assignment.onrender.com/auth/google';
+  //   });
+
+  //   const signinButton2 = document.getElementById('signin-btn-ms');
+  
+  //   signinButton2.addEventListener('click', function() {
+      
+  //   //   window.location.href = 'https://reachinbox-assignment-4rf9.onrender.com/signin';
+  //   });
+  // });
